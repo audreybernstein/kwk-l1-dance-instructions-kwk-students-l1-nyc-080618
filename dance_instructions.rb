695cb369b7a851ddbeb0b9ae7422ce1473ed5737
audreybernstein <audrey.bernstein@aol.com>
2018-08-07 14:01:23 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -30,9 +30,9 @@ end
 def gangnam_style
   starting_stance
   base_footwork
-  skip_step("right")
+  skip_step("right foot")
   bob_the_reins
-  skip_step("left")
+  skip_step("left foot")
   lasso
   bust_a_move
 end
