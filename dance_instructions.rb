a433ee5ac1dffdeaab9de82e88b7a5c9ceb36978
audreybernstein <audrey.bernstein@aol.com>
2018-08-07 13:53:44 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -11,3 +11,12 @@ def skip_step(starting_foot)
   puts "Bounce #{starting_foot} back up slightly, kicking it a few inches back"
   puts "Briefly raise the opposite foot, then the #{starting foot} twice more!"
 end
+def bob_the_reins
+  puts "Holding your arms out in front of you, straight and at chest level"
+  puts "Cross your right wrist over your left and hold them together"
+  puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
+  puts "Do this 7 more times!"
+end
+def lasso
+  puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
+end
