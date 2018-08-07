d67dae619a948bea16e2ed1517179ad2435e8ebd
audreybernstein <audrey.bernstein@aol.com>
2018-08-07 14:01:46 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -9,7 +9,7 @@ end
 def skip_step(starting_foot)
   puts "Lower #{starting_foot} to the ground"
   puts "Bounce #{starting_foot} back up slightly, kicking it a few inches back"
-  puts "Briefly raise the opposite foot, then the #{starting foot} twice more!"
+  puts "Briefly raise the opposite foot, then the #{starting_foot} twice more!"
 end
 def bob_the_reins
   puts "Holding your arms out in front of you, straight and at chest level"
